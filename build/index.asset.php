a6484520f9e91dd7dad57e5915fa962944bbf4d3
kripanath <kraditya2006@gmail.com>
2024-10-11 16:33:28 +0600

    Block setup done

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '2f9bb073f3b62ae5ef5035738ca201e6');
